eb574cba556510fafecfcd08730bfd953ac7b133
niladreeOwO <niladree.bardhan360@gmail.com>
2025-12-01 05:29:23 +0600

    Create transpose-README.py

---
+++ transpose-README.py
@@ -0,0 +1,37 @@
+Rectangular Matrix
+Input:
+2 3
+1 2 3
+4 5 6
+
+output:
+rectangular matrix
+
+
+1 2 3 
+4 5 6 
+
+
+1 4 
+2 5 
+3 6 
+
+Square Mateix
+Input:
+3 3
+1 2 3
+4 5 6
+7 8 9
+
+Output:
+square matrix
+
+
+1 2 3 
+4 5 6 
+7 8 9 
+
+
+1 4 7 
+2 5 8 
+3 6 9 
